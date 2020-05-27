75a1f07b3168dbf2172de2532020c9c028e276ef
Jonathan <30645732+jtmullen@users.noreply.github.com>
2020-05-26 21:29:52 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,8 +3,8 @@
 echo "I am here"
 echo "Input Branch: $INPUT_BRANCH"
 cd "$GITHUB_WORKSPACE"
-BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
-echo "Found branch $BRANCH_NAME"
+# BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
+# echo "Found branch $BRANCH_NAME"
 
 echo "::set-output name=fails::0"
 
