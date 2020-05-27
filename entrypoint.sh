3e7fbd1d35949e7395b4cdeda9c76c4504d8bf12
Jonathan <30645732+jtmullen@users.noreply.github.com>
2020-05-26 22:16:45 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,14 @@ echo "Status $GIT_STATUS"
 BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
 echo "Found branch $BRANCH_NAME"
 
+GIT_HASH=`git rev-parse`
+HEAD_HASH=`git rev-parse HEAD`
+echo "Hash: $GIT_HASH  -  $HEAD_HASH"
+cd common/api/CAN
+GIT_HASH=`git rev-parse`
+HEAD_HASH=`git rev-parse HEAD`
+echo "CAN Hash: $GIT_HASH  -  $HEAD_HASH"
+
 echo "::set-output name=fails::0"
 
 exit 0
