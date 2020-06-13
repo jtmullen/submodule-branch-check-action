070b694d39e033f7ef1e5fef5ace317607ebd574
Jonathan <30645732+jtmullen@users.noreply.github.com>
2020-06-13 14:35:18 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,17 +6,17 @@ REPO=`jq -r ".repository.full_name" "${GITHUB_EVENT_PATH}"`
 
 cd "${GITHUB_WORKSPACE}"
 
-PR=$(jq -r ".number" "${GITHUB_EVENT_PATH}")
+PR=`jq -r ".number" "${GITHUB_EVENT_PATH}"`
 echo "PR: ${PR}"
 
 BASE_URI="https://api.github.com"
 API_HEADER="Accept: application/vnd.github.v3+json"
 AUTH_HEADER="Authorization: token ${GITHUB_TOKEN}"
 
-PR_INFO=$(curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
-  "${BASE_URI}/repos/${REPO}/pulls/${PR}")
+PR_INFO=`curl -X GET -s -H "${AUTH_HEADER}" -H "${API_HEADER}" \
+  "${BASE_URI}/repos/${REPO}/pulls/${PR}"`
 
-REF=$(echo "${PR_INFO}" | jq -r .head.ref)
+REF=`echo "${PR_INFO}" | jq -r .head.ref`
 
 
 git config --global user.email "submodule@github.com"
