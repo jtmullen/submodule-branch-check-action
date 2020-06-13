d6c5c7673c6b84ddc5999f8ee463bc24cac0b1a9
Jonathan <30645732+jtmullen@users.noreply.github.com>
2020-06-13 12:47:58 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,9 @@
 #!/bin/sh -l
 
 echo "I am here"
-echo "Input Branch: $INPUT_BRANCH"
-cd "$GITHUB_WORKSPACE"
-GIT_STATUS=`git status`
-echo "Status $GIT_STATUS"
-BRANCH_NAME=`git rev-parse --abbrev-ref HEAD`
-echo "Found branch $BRANCH_NAME"
 
-GIT_HASH=`git rev-parse`
-HEAD_HASH=`git rev-parse HEAD`
-echo "Hash: $GIT_HASH  -  $HEAD_HASH"
-cd common/api/CAN
-GIT_HASH=`git rev-parse`
-HEAD_HASH=`git rev-parse HEAD`
-echo "CAN Hash: $GIT_HASH  -  $HEAD_HASH"
+jq . "${GITHUB_EVENT_PATH}"
+
 
 echo "::set-output name=fails::0"
 
