da9f51fbb72a8acd316548bd96d8619effcd1f6e
Jonathan <30645732+jtmullen@users.noreply.github.com>
2020-08-25 21:54:02 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -28,7 +28,7 @@ cd "${GITHUB_WORKSPACE}" || error "Error: Cannot change directory to Github Work
 SUBMODULES=`git config --file .gitmodules --name-only --get-regexp path`
 echo "${SUBMODULES}" | grep ".${INPUT_PATH}." || error "Error: path is not a submodule"
 
-echo `ls`
+echo `git branch`
 git checkout "${TO_REF}"
 git submodule init "${INPUT_PATH}"
 git submodule update "${INPUT_PATH}"
