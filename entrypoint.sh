695d696af4477827d88b02c7c1d5d7dbb64d70c9
Jonathan <30645732+jtmullen@users.noreply.github.com>
2020-09-19 00:46:28 -0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -66,7 +66,9 @@ if [[ ! -z "${INPUT_PASS_IF_UNCHANGED}" ]]; then
 	echo "Check if submodule has been changed on ${TO_REF}"
 	CHANGED=`git diff --name-only origin/${FROM_REF}...origin/${TO_REF}`
 	echo "${CHANGED}"
-	echo "${CHANGED}" | grep "${INPUT_PATH}" | pass "Submodule ${INPUT_PATH} has not been changed on ${TO_REF}"
+	if ! grep -q "^${INPUT_PATH}$" "${CHANGED}"; then
+		pass "Submodule ${INPUT_PATH} has not been changed on ${TO_REF}"
+	fi
 	echo "Submodule has been changed"
 fi
 
