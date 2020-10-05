1e46870fdef3e6fc43fbf171ac43dccecb2607ca
Jonathan <30645732+jtmullen@users.noreply.github.com>
2020-10-04 23:53:43 -0500

    Checkout by hash (#12)
    * Checkout by SHA for PR and add user
    
    * Fix hash vs branch name usage
    
    * Cleanup

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,15 +10,22 @@ REPO=`jq -r ".repository.full_name" "${GITHUB_EVENT_PATH}"`
 isPR=false
 if [[ $(jq -r ".pull_request.head.ref" "${GITHUB_EVENT_PATH}") != "null" ]]; then
 	PR=`jq -r ".number" "${GITHUB_EVENT_PATH}"`
-	TO_REF=`jq -r ".pull_request.head.ref" "${GITHUB_EVENT_PATH}"`
-	FROM_REF=`jq -r ".pull_request.base.ref" "${GITHUB_EVENT_PATH}"`
-	echo "Run for PR # ${PR} of ${TO_REF} into ${FROM_REF} on ${REPO}"
+	TO_HASH=`jq -r ".pull_request.head.sha" "${GITHUB_EVENT_PATH}"`
+	FROM_HASH=`jq -r ".pull_request.base.sha" "${GITHUB_EVENT_PATH}"`
+	PR_BRANCH=`jq -r ".pull_request.head.ref" "${GITHUB_EVENT_PATH}"`
+	BASE_BRANCH=`jq -r ".pull_request.base.ref" "${GITHUB_EVENT_PATH}"`
+	USER=`jq -r ".pull_request.user.login" "${GITHUB_EVENT_PATH}"`
+	echo "Run for PR # ${PR} of ${PR_BRANCH} into ${BASE_BRANCH} on ${REPO} by ${USER}"
+	echo "Hash ${TO_HASH} into ${FROM_HASH}"
 	isPR=true
 elif [[ $(jq -r ".after" "${GITHUB_EVENT_PATH}") != "null" ]]; then
-	TO_REF=`jq -r ".after" "${GITHUB_EVENT_PATH}"`
-	FROM_REF=`jq -r ".before" "${GITHUB_EVENT_PATH}"`
+	TO_HASH=`jq -r ".after" "${GITHUB_EVENT_PATH}"`
+	FROM_HASH=`jq -r ".before" "${GITHUB_EVENT_PATH}"`
 	BRANCH_NAME=`jq -r ".ref" "${GITHUB_EVENT_PATH}"`
-	echo "Run for push of ${BRANCH_NAME} from ${FROM_REF} to ${TO_REF} on ${REPO}"
+	BASE_BRANCH="${FROM_HASH}"
+	PR_BRANCH="${TO_HASH}"
+	USER=`jq -r ".pusher.name" "${GITHUB_EVENT_PATH}"`
+	echo "Run for push of ${BRANCH_NAME} from ${FROM_HASH} to ${TO_HASH} on ${REPO} by ${USER}"
 else
 	error "Unknown Github Event Path"
 fi
@@ -30,32 +37,31 @@ if [[ "${isPR}" = true ]]; then
 	echo "Fetch Branch Histories"
 	if [[ ! -z "${INPUT_FETCH_DEPTH}" ]]; then
 		echo "Histories to depth: ${INPUT_FETCH_DEPTH}"
-		git fetch origin "${TO_REF}" --recurse-submodules=no --depth "${INPUT_FETCH_DEPTH}" || error "__Line:${LINENO}__Error: Could not fetch history of ${TO_REF}"
-		git fetch origin "${FROM_REF}" --recurse-submodules=no --depth "${INPUT_FETCH_DEPTH}" || error "__Line:${LINENO}__Error: Could not fetch history of ${FROM_REF}"
+		git fetch origin "${PR_BRANCH}" --recurse-submodules=no --depth "${INPUT_FETCH_DEPTH}" || error "__Line:${LINENO}__Error: Could not fetch history of ${PR_BRANCH}"
+		git fetch origin "${BASE_BRANCH}" --recurse-submodules=no --depth "${INPUT_FETCH_DEPTH}" || error "__Line:${LINENO}__Error: Could not fetch history of ${BASE_BRANCH}"
 	else
 		echo "Full Brach Histories"
-		git fetch origin --recurse-submodules=no "${TO_REF}" || error "__Line:${LINENO}__Error: Could not fetch history of ${TO_REF}"
-		git fetch origin --recurse-submodules=no "${FROM_REF}" || error "__Line:${LINENO}__Error: Could not fetch history of ${FROM_REF}"
+		git fetch origin --recurse-submodules=no "${PR_BRANCH}" || error "__Line:${LINENO}__Error: Could not fetch history of ${PR_BRANCH}"
+		git fetch origin --recurse-submodules=no "${BASE_BRANCH}" || error "__Line:${LINENO}__Error: Could not fetch history of ${BASE_BRANCH}"
 	fi
 fi
 
 ## Check for submodule valid
-echo "Submodule and Tree Info"
 SUBMODULES=`git config --file .gitmodules --name-only --get-regexp path`
 echo "${SUBMODULES}" | grep ".${INPUT_PATH}." || error "Error: path \"${INPUT_PATH}\" is not a submodule"
 
-git checkout "${TO_REF}" || error "__Line:${LINENO}__Error: Could not checkout ${TO_REF}"
+git checkout "${TO_HASH}" || error "__Line:${LINENO}__Error: Could not checkout ${TO_HASH}"
 git submodule init "${INPUT_PATH}" || error "__Line:${LINENO}__Error: Could initialize submodule"
-git submodule update "${INPUT_PATH}" || error "__Line:${LINENO}__Error: Could not checkout submodule hash referenced by ${TO_REF} (is it pushed to remote?)"
+git submodule update "${INPUT_PATH}" || error "__Line:${LINENO}__Error: Could not checkout submodule hash referenced by ${PR_BRANCH} (is it pushed to remote?)"
 
 echo "Switch to submodule at: ${INPUT_PATH}"
 cd "${INPUT_PATH}" || error "__Line:${LINENO}__Error: Cannot change directory to the submodule"
 SUBMODULE_HASH=`git rev-parse HEAD`
 
 cd "${GITHUB_WORKSPACE}" || error "__Line:${LINENO}__Error: Cannot change directory to Github Workspace" 
-git checkout "${FROM_REF}"  || error "__Line:${LINENO}__Error: Could not checkout ${FROM_REF}"
+git checkout "${FROM_HASH}"  || error "__Line:${LINENO}__Error: Could not checkout ${FROM_HASH}"
 
-git submodule update "${INPUT_PATH}"  || error "__Line:${LINENO}__Error: Could not checkout submodule hash referenced by ${FROM_REF} (is it pushed to remote?)"
+git submodule update "${INPUT_PATH}"  || error "__Line:${LINENO}__Error: Could not checkout submodule hash referenced by ${BASE_BRANCH} (is it pushed to remote?)"
 
 cd "${INPUT_PATH}" || error "__Line:${LINENO}__Error: Cannot change directory to the submodule"
 SUBMODULE_HASH_BASE=`git rev-parse HEAD`
@@ -79,10 +85,10 @@ cd "${GITHUB_WORKSPACE}" || error "__Line:${LINENO}__Error: Cannot change direct
 ## Pass if they are unchanged
 if [[ ! -z "${INPUT_PASS_IF_UNCHANGED}" ]]; then
 	if [[ "${isPR}" = true ]]; then 
-		echo "Check if submodule has been changed on ${TO_REF}"
-		CHANGED=`git diff --name-only origin/${FROM_REF}...origin/${TO_REF}`
+		echo "Check if submodule has been changed on ${PR_BRANCH}"
+		CHANGED=`git diff --name-only origin/${FROM_HASH}...origin/${TO_HASH}`
 		if ! grep "^${INPUT_PATH}$" <<< "${CHANGED}"; then
-			pass "Submodule ${INPUT_PATH} has not been changed on branch ${TO_REF}"
+			pass "Submodule ${INPUT_PATH} has not been changed on branch ${PR_BRANCH}"
 		fi
 		echo "Submodule has been changed"
 	else
@@ -103,11 +109,11 @@ fi
 ## If they are the same pass
 echo "Check if submodule unchanged"
 if [ "${master_lib_hash}" == "${lib_hash}" ]; then
-    pass "${INPUT_PATH} is the same as ${FROM_REF}"
+    pass "${INPUT_PATH} is the same as ${BASE_BRANCH}"
 fi
 
 ## Check that base hash is an ancestor of the ref hash
 echo "Check if old submodule has is parent of current"
-git rev-list "${SUBMODULE_HASH}" | grep "${SUBMODULE_HASH_BASE}" || fail "Submodule ${INPUT_PATH} on ${FROM_REF} is not an ancestor of that on ${TO_REF}"
+git rev-list "${SUBMODULE_HASH}" | grep "${SUBMODULE_HASH_BASE}" || fail "Submodule ${INPUT_PATH} on ${BASE_BRANCH} is not an ancestor of that on ${PR_BRANCH}"
 
-pass "Valid submodule ${INPUT_PATH} on ${TO_REF}"
+pass "Valid submodule ${INPUT_PATH} on ${PR_BRANCH}"
