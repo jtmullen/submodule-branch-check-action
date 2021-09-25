f959cb0ceab76e51f106bf6d6a3f76bd6d5056bd
Jonathan <30645732+jtmullen@users.noreply.github.com>
2021-09-24 19:44:08 -0500

    v1 Release - Cleanup, Tests, & Added Options (#16)

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
 
 error () {
+	echo "::set-output name=fails::error"
 	echo "::error::$1"
 	exit 1
 }
 
+# When a new submodule is added we have no way to check for progression. In this case we will skip that check (instead of failing it)
 newSubmodule=false
-
 newSubmoduleWarning() {
 	newSubmodule=true
 	echo "::warning::Submodule $1 does not exist on the base branch/commit;  Cannot do progression check for new submodules"
 }
 
-REPO=`jq -r ".repository.full_name" "${GITHUB_EVENT_PATH}"`
+## Path to JSON Payload of the Github Event Info
+EVENT_PATH=GITHUB_EVENT_PATH
 
+## Check for override of event path - used for tests
+if [[ ! -z "${INPUT_EVENT_PATH}" ]]; then
+	EVENT_PATH="${INPUT_EVENT_PATH}"
+	## Put warning here as this probably should not be used outside testing
+	echo "::warning::Github Event Payload Path Overwritten. Only intended for testing use."
+fi
+
+echo "::group::Setup"
+
+cd "${GITHUB_WORKSPACE}" || error "__Line:${LINENO}__Error: Cannot change directory to Github Workspace"
+
+REPO=`jq -r ".repository.full_name" "${EVENT_PATH}"`
+
+## Get all the info about this push or pr event we need to check the submodule
 isPR=false
-if [[ $(jq -r ".pull_request.head.ref" "${GITHUB_EVENT_PATH}") != "null" ]]; then
-	PR=`jq -r ".number" "${GITHUB_EVENT_PATH}"`
-	PR_BRANCH=`jq -r ".pull_request.head.ref" "${GITHUB_EVENT_PATH}"`
-	BASE_BRANCH=`jq -r ".pull_request.base.ref" "${GITHUB_EVENT_PATH}"`
-	USER=`jq -r ".pull_request.user.login" "${GITHUB_EVENT_PATH}"`
+if [[ $(jq -r ".pull_request.head.ref" "${EVENT_PATH}") != "null" ]]; then
+	PR=`jq -r ".number" "${EVENT_PATH}"`
+	PR_BRANCH=`jq -r ".pull_request.head.ref" "${EVENT_PATH}"`
+	BASE_BRANCH=`jq -r ".pull_request.base.ref" "${EVENT_PATH}"`
+	USER=`jq -r ".pull_request.user.login" "${EVENT_PATH}"`
 	git fetch origin "${PR_BRANCH}" --recurse-submodules=no --depth 1 || error "__Line:${LINENO}__Error: Could not fetch tip of ${PR_BRANCH}"
 	git fetch origin "${BASE_BRANCH}" --recurse-submodules=no --depth 1 || error "__Line:${LINENO}__Error: Could not fetch tip of ${BASE_BRANCH}"
 	TO_HASH=`git rev-parse origin/${PR_BRANCH}`
@@ -27,22 +43,22 @@ if [[ $(jq -r ".pull_request.head.ref" "${GITHUB_EVENT_PATH}") != "null" ]]; the
 	echo "Run for PR # ${PR} of ${PR_BRANCH} into ${BASE_BRANCH} on ${REPO} by ${USER}"
 	echo "Hash ${TO_HASH} into ${FROM_HASH}"
 	isPR=true
-elif [[ $(jq -r ".after" "${GITHUB_EVENT_PATH}") != "null" ]]; then
-	TO_HASH=`jq -r ".after" "${GITHUB_EVENT_PATH}"`
-	FROM_HASH=`jq -r ".before" "${GITHUB_EVENT_PATH}"`
-	BRANCH_NAME=`jq -r ".ref" "${GITHUB_EVENT_PATH}"`
+elif [[ $(jq -r ".after" "${EVENT_PATH}") != "null" ]]; then
+	TO_HASH=`jq -r ".after" "${EVENT_PATH}"`
+	FROM_HASH=`jq -r ".before" "${EVENT_PATH}"`
+	BRANCH_NAME=`jq -r ".ref" "${EVENT_PATH}"`
 	BASE_BRANCH="${FROM_HASH}"
 	PR_BRANCH="${TO_HASH}"
-	USER=`jq -r ".pusher.name" "${GITHUB_EVENT_PATH}"`
+	USER=`jq -r ".pusher.name" "${EVENT_PATH}"`
 	echo "Run for push of ${BRANCH_NAME} from ${FROM_HASH} to ${TO_HASH} on ${REPO} by ${USER}"
 else
-	error "Unknown Github Event Path"
+	## Doesn't appear to be a PR or a push
+	error "Unknown Github Event Payload"
 fi
-
-cd "${GITHUB_WORKSPACE}" || error "__Line:${LINENO}__Error: Cannot change directory to Github Workspace"
 
 ## Fetch both branches for PR
 if [[ "${isPR}" = true ]]; then
+	echo "::group::Get PR Branches"
 	echo "Fetch Branch Histories"
 	if [[ ! -z "${INPUT_FETCH_DEPTH}" ]]; then
 		echo "Histories to depth: ${INPUT_FETCH_DEPTH}"
@@ -53,15 +69,30 @@ if [[ "${isPR}" = true ]]; then
 		git fetch origin --recurse-submodules=no "${PR_BRANCH}" || error "__Line:${LINENO}__Error: Could not fetch history of ${PR_BRANCH}"
 		git fetch origin --recurse-submodules=no "${BASE_BRANCH}" || error "__Line:${LINENO}__Error: Could not fetch history of ${BASE_BRANCH}"
 	fi
+	echo "::endgroup::"
 fi
+
+echo "::endgroup::"
+echo "::group::Get Submodule Info"
+
+
+## Move to end hash when setting up submodule, otherwise would fail if submodule does not exist on whetever was originally checked out
+git checkout "${TO_HASH}" || error "__Line:${LINENO}__Error: Could not checkout ${TO_HASH}"
 
 ## Check for submodule valid
 SUBMODULES=`git config --file .gitmodules --name-only --get-regexp path`
-echo "${SUBMODULES}" | grep ".${INPUT_PATH}." || error "Error: path \"${INPUT_PATH}\" is not a submodule"
+echo "${SUBMODULES}" | grep ".${INPUT_PATH}." || error "Error: path \"${INPUT_PATH}\" is not a submodule on ${TO_HASH}"
 
-git checkout "${TO_HASH}" || error "__Line:${LINENO}__Error: Could not checkout ${TO_HASH}"
 git submodule init "${INPUT_PATH}" || error "__Line:${LINENO}__Error: Could initialize submodule"
-git submodule update "${INPUT_PATH}" || error "__Line:${LINENO}__Error: Could not checkout submodule hash referenced by ${PR_BRANCH} (is it pushed to remote?)"
+
+## Update Submodule 
+if [[ ! -z "${INPUT_SUB_FETCH_DEPTH}" ]]; then
+	echo "Submodule History to depth: ${INPUT_SUB_FETCH_DEPTH}"
+	git submodule update "${INPUT_PATH}" --depth "${INPUT_SUB_FETCH_DEPTH}" || error "__Line:${LINENO}__Error: Could not checkout submodule hash referenced by ${PR_BRANCH} (is it pushed to remote?)"
+else
+	echo "Full Submodule History"
+	git submodule update "${INPUT_PATH}" || error "__Line:${LINENO}__Error: Could not checkout submodule hash referenced by ${PR_BRANCH} (is it pushed to remote?)"
+fi
 
 cd "${INPUT_PATH}" || error "__Line:${LINENO}__Error: Cannot change directory to the submodule"
 SUBMODULE_HASH=`git rev-parse HEAD`
@@ -97,8 +128,11 @@ pass () {
 
 cd "${GITHUB_WORKSPACE}" || error "__Line:${LINENO}__Error: Cannot change directory to Github Workspace" 
 
-## Pass if they are unchanged
+echo "::endgroup::"
+
+## Immediately pass if submodule is not changed (PR only)
 if [[ ! -z "${INPUT_PASS_IF_UNCHANGED}" ]]; then
+	echo "::group::Check if Unchanged"
 	if [[ "${isPR}" = true ]]; then 
 		echo "Check if submodule has been changed on ${PR_BRANCH}"
 		CHANGED=`git diff --name-only ${FROM_HASH}...${TO_HASH}`
@@ -109,12 +143,14 @@ if [[ ! -z "${INPUT_PASS_IF_UNCHANGED}" ]]; then
 	else
 		echo "Note: Not a PR - Pass if Unchanged ignored"
 	fi	
+	echo "::endgroup::"
 fi
 
 cd "${INPUT_PATH}" || error "__Line:${LINENO}__Error: Cannot change directory to the submodule"
 
 ## Check if most recent required
 if [[ ! -z "${INPUT_REQUIRE_HEAD}" ]]; then
+	echo "::group::Check if Most Recent"
 	echo "Check if on most recent"
 	if [[ -z "${INPUT_BRANCH}" ]]; then
 		error "Error: Branch not provided but Head Commit is required"
@@ -125,19 +161,23 @@ if [[ ! -z "${INPUT_REQUIRE_HEAD}" ]]; then
 	else
 		fail "Submodule ${INPUT_PATH} is not on most recent ${INPUT_BRANCH} ($HEAD_SHA)"
 	fi
+	echo "::endgroup::"
 fi
 
 ## Check if on required branch
 if [[ ! -z "${INPUT_BRANCH}" ]]; then
+	echo "::group::Check Required Branch"
 	echo "Check for submodule on branch ${INPUT_BRANCH}"
 	BRANCHES=`git branch -r --contains ${SUBMODULE_HASH}`
 	echo "${BRANCHES}" | grep "/${INPUT_BRANCH}$" || fail "Submodule ${INPUT_PATH} Hash ${SUBMODULE_HASH} is not on branch ${INPUT_BRANCH}"
 	echo "Submodule is on branch ${INPUT_BRANCH}"
+	echo "::endgroup::"
 fi
 
 
 ##only check for progression if we have something to compare against
 if [ "$newSubmodule" = false ]; then
+	echo "::group::Check Progression"
 	## If they are the same pass
 	echo "Check if submodule is identical hash"
 	if [ "${SUBMODULE_HASH_BASE}" == "${SUBMODULE_HASH}" ]; then
@@ -147,6 +187,7 @@ if [ "$newSubmodule" = false ]; then
 	## Check that base hash is an ancestor of the ref hash
 	echo "Verify old submodule hash is ancestor of current"
 	git rev-list "${SUBMODULE_HASH}" | grep "${SUBMODULE_HASH_BASE}" || fail "Submodule ${INPUT_PATH} on ${BASE_BRANCH} is not an ancestor of that on ${PR_BRANCH}"
+	echo "::endgroup::"
 fi
 
 pass "Valid submodule ${INPUT_PATH} on ${PR_BRANCH}"
