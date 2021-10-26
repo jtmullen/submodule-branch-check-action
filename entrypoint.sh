1b2ae9550b4d4ee56c27eb8005a4219f896b3848
Jonathan <30645732+jtmullen@users.noreply.github.com>
2021-10-25 22:13:20 -0500

    verbose

--- entrypoint.sh
+++ entrypoint.sh
@@ -83,8 +83,10 @@ git checkout "${TO_HASH}" || error "__Line:${LINENO}__Error: Could not checkout 
 SUBMODULES=`git config --file .gitmodules --name-only --get-regexp path`
 echo "${SUBMODULES}" | grep ".${INPUT_PATH}." || error "Error: path \"${INPUT_PATH}\" is not a submodule on ${TO_HASH}"
 
+git --version
+
 ## Initialize Submodule
-git submodule update --init --depth=1 --no-single-branch "${INPUT_PATH}" || error "__Line:${LINENO}__Error: Could not initialize submodule ${INPUT_PATH} as referenced by ${PR_BRANCH} (is the referenced commit pushed to remote?)"
+git submodule update -v --init --depth=1 --no-single-branch "${INPUT_PATH}" || error "__Line:${LINENO}__Error: Could not initialize submodule ${INPUT_PATH} as referenced by ${PR_BRANCH} (is the referenced commit pushed to remote?)"
 cd "${INPUT_PATH}" || error "__Line:${LINENO}__Error: Cannot change directory to the submodule"
 SUBMODULE_HASH=`git rev-parse HEAD`
 
