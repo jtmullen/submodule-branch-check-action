529cbc4493eda283d1f4614ce640e980f51cff3f
Jonathan <30645732+jtmullen@users.noreply.github.com>
2022-03-01 10:33:51 -0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -92,6 +92,7 @@ git rev-list "${SUBMODULE_HASH}"
 
 ## Need to get all remote branches, we don't know what we don't know about the submodule
 git remote set-branches origin '*' || error "__Line:${LINENO}__Error: Could not set branches"
+git checkout "${SUBMODULE_HASH}" || error "__Line:${LINENO}__Error: Cannot checkout submodule hash"
 
 ## Update Submodule 
 if [[ ! -z "${INPUT_SUB_FETCH_DEPTH}" ]]; then
